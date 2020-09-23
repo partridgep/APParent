b99d519d470db0833cfc5cfd0898d128f17fb190
Henry Northcote <hnorthcote2000@hotmail.com>
2020-09-22 21:48:13 -0600

    saving progress

--- main_app/models.py
+++ main_app/models.py
@@ -117,6 +117,10 @@ class Goal(models.Model):
     def __str__(self):
         return f'{self.title}: ({self.child})'
 
+    @property
+    def days_left(self):
+        return (self.created_at - datetime.today()).days + 1
+
     class Meta:
         ordering = ['deadline']
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Child, Picture
+from .models import Child, Picture, Goal
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
@@ -7,7 +7,7 @@ from django.contrib.auth import update_session_auth_hash
 from .forms import ParentSignUpForm, NotParentSignUpForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+from datetime import datetime
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
@@ -17,6 +17,7 @@ import boto3
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'seir-apparent'
 
+TRACKER = (('1', 'Completed'), ('2', 'On track'), ('3','Behind schedule'))
 # HELPER FUNCTION
 
 def generate_username(email):
@@ -386,4 +387,55 @@ def edit_password(request):
 
     return render(request, 'registration/edit_password.html', {
         'form': form
+    })
+
+def goals_index(request):
+    user = request.user
+    goals= Goal.objects.all()
+    print(user)
+    return render(request, 'goals/index.html', {'user':user, 'goals':goals})
+
+@login_required
+def add_goal(request):
+    user = request.user
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        created_at = datetime.today()
+        # created_by = user.id
+        # child = child_id
+        goal_tracker = request.POST.get("goal_tracker")
+        deadline = request.POST.get("deadline")
+        goal = Goal(title=title, description=description, created_at=created_at, goal_tracker=goal_tracker, deadline=deadline)
+        goal.save()
+
+        print(goal)
+        return redirect('goal_detail', goal_id=goal.id)
+    goal_tracker = TRACKER
+    print(goal_tracker)
+    return render(request, 'goals/add.html', {'goal_tracker': goal_tracker})
+
+
+@login_required
+def goal_detail(request, goal_id):
+    goal = Goal.objects.get(id=goal_id)
+    current_user = request.user
+    return render(request, 'goals/detail.html', {
+        'goal': goal,
+    })
+
+@login_required
+def goal_edit(request, goal_id):
+    goal = Goal.objects.get(id=goal_id)
+    if request.method == "POST":
+        goal.title = request.POST.get("title")
+        goal.description = request.POST.get("description")
+        goal.goal_tracker = request.POST.get("goal_tracker")
+        goal.deadline = request.POST.get("deadline")
+        goal.save()
+
+        print(goal_edit)
+        return redirect('goal_detail', goal_id=goal.id)
+    return render(request, 'goals/edit.html', {
+        'goal': goal,
     })
